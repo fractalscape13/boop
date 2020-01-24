9bc4d62bc2850220f1dd89e726cac5529a9bd18d
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-24 09:33:00 -0800

    update show/hide to include header text, change padding

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,16 @@ $(document).ready(function() {
     var input = parseInt($("input").val());
     beepboop(input);
     $("form").hide();
+    $("h1").hide();
     $("#output").show();
     $("#result").text(finalOutput);
   });
 });
 
 
-var finalOutput = [];
+
+//business logic
+var finalOutput = [0,1,2,3];
 
 function beepboop(num) {
 
